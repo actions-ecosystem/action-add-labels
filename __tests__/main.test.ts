13b46add6f90cc2f66e413d3a1f734966553bf56
micnncim <micnncim@gmail.com>
2020-05-10 10:19:51 +0900

    Refactor

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,19 +1,3 @@
-import * as Processor from '../src/Processor';
-
-test('issues payload is assumed to be issues payload', async () => {
-  const payload: any = {
-    pull_request: {
-      number: 1
-    }
-  };
-  expect(Processor.isWebhookPayloadPullRequest(payload)).toEqual(true);
-});
-
-test('pull request payload is assumed to be pull request payload', async () => {
-  const payload: any = {
-    issue: {
-      number: 1
-    }
-  };
-  expect(Processor.isWebhookPayloadIssues(payload)).toEqual(true);
+describe('TODO - Add a test suite', () => {
+  it('TODO - Add a test', async () => {});
 });
