321fdd1e7344d54ab8eef2b23304f79efbc45263
Peter Marathas <peter.marathas@snapsheet.me>
2024-03-08 16:18:29 -0500

    Allow specifying colors

--- src/main.ts
+++ src/main.ts
@@ -20,12 +20,41 @@ async function run(): Promise<void> {
     }
 
     const octokit = github.getOctokit(githubToken);
-    await octokit.rest.issues.addLabels({
+    const { data: githubLabels } = await octokit.rest.issues.addLabels({
       labels,
       owner,
       repo,
       issue_number: number
     });
+    const githubLabelsByName: { [label: string]: (typeof githubLabels)[0] } = githubLabels
+      .reduce((acc, label) => ({ ...acc, [label.name]: label }), {});
+
+    const colors = core
+      .getInput('colors')
+      .split('\n')
+      .filter(c => c !== '')
+      .slice(0, labels.length);
+
+    for (const [i, color] of colors.entries()) {
+      const name = labels[i];
+      const githubLabel = githubLabelsByName[name];
+      if (!githubLabel || githubLabel.color == color) {
+        continue;
+      }
+      try {
+        await octokit.rest.issues.updateLabel({
+          owner,
+          repo,
+          name,
+          color
+        });
+        core.info(`Updated color for label "${githubLabel.name}" from #${githubLabel.color} to #${color}`);
+      } catch (e) {
+        if (e instanceof Error) {
+          core.error(e);
+        }
+      }
+    }
   } catch (e) {
     if (e instanceof Error) {
       core.error(e);
